21ad1bd7beef35ce2a1858dfef093b9dd337cf1e
Silver Kuusik <silver.kuusik@gmail.com>
2014-11-14 22:07:24 +0100

    added arduino code

---
+++ petricity.ino
@@ -0,0 +1,84 @@
+#include <Servo.h>
+
+/* Initialize the Servo objects */
+Servo leftLeg;
+Servo rightLeg;
+
+/* Moves the pet forwards */
+void forward() {
+    leftLeg.write(80);
+    delay(250);
+    rightLeg.write(40);
+    delay(250);
+    leftLeg.write(110);
+    delay(250);
+    rightLeg.write(60);
+    delay(250);
+}
+
+/* Moves the pet backwards */
+void backward() {
+    leftLeg.write(140);
+    delay(250);
+    rightLeg.write(90);
+    delay(250);
+    leftLeg.write(110);
+    delay(250);
+    rightLeg.write(60);
+    delay(250);
+}
+
+/* Makes the pet's eye red */
+void red() {
+    analogWrite(9, 0);
+    analogWrite(10, 255);
+    analogWrite(11, 255);
+}
+
+/* Makes the pet's eye green */
+void green() {
+    analogWrite(9, 255);
+    analogWrite(10, 0);
+    analogWrite(11, 255);
+}
+
+/* Makes the pet's eye blue */
+void blue() {
+    analogWrite(9, 255);
+    analogWrite(10, 255);
+    analogWrite(11, 0);
+}
+
+setup () {
+    /* Initialize Servomotors */
+    leftLeg.attach(5);
+    rightLeg.attach(6);
+    /* Initial position for the legs */
+    leftLeg.write(110);
+    rightLeg.write(40);
+    /* Initialize serial communication */
+    Serial.begin(9600);
+}
+
+loop() {
+    if (Serial.available()) {
+        char c = Serial.read();
+        switch (c) {
+            case 'w':
+                forward();
+                break;
+            case 's':
+                backward();
+                break;
+            case 'r':
+                red();
+                break;
+            case 'g':
+                green();
+                break;
+            case 'b':
+                blue();
+                break;
+        }
+    }
+}
